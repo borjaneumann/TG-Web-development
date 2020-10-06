2c71d80dca1d8fe75de7d1bbaf7c42dfd3b1b653
Borja <borjaneumann@hotmail.com>
2020-10-06 12:59:33 +0200

    Soft skills and Javascript update

---
+++ 05_JAVASCRIPT/06_fuctions/08_func_opdr.js
@@ -0,0 +1,17 @@
+// document.body.style.color='red';
+
+// document.getElementById('w1').style.backgroundColor='green';
+// document.getElementById('w2').style.backgroundColor='red';
+// document.getElementById('w3').style.backgroundColor='lightgreen';
+// let text = 'hola';
+// document.getElementById('w3').innerHTML=text;
+
+//table Wiskunde
+let (input);
+input = document.getElementById('w1').value;
+    if (input == 5){
+        // input = parseInt(input)
+        
+
+        document.getElementById('w1').style.backgroundColor='red';
+    }
