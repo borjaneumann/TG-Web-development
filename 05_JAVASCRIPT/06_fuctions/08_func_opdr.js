77b4cd111d6b1c93db2a9f873093d217d0c7eb97
Borja <borjaneumann@hotmail.com>
2020-10-08 10:46:24 +0200

    Javascript t/m arrays afgemaakt

--- 05_JAVASCRIPT/06_fuctions/08_func_opdr.js
+++ 05_JAVASCRIPT/06_fuctions/08_func_opdr.js
@@ -1,17 +1,362 @@
-// document.body.style.color='red';
 
-// document.getElementById('w1').style.backgroundColor='green';
-// document.getElementById('w2').style.backgroundColor='red';
-// document.getElementById('w3').style.backgroundColor='lightgreen';
-// let text = 'hola';
-// document.getElementById('w3').innerHTML=text;
 
 //table Wiskunde
-let (input);
-input = document.getElementById('w1').value;
-    if (input == 5){
-        // input = parseInt(input)
-        
+//table Wiskunde
+let input;
 
+function wiskunde () {
+    input = document.getElementById('w1').value;
+    if (input>0 && input < 5.5){
         document.getElementById('w1').style.backgroundColor='red';
+        document.getElementById('w1').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('w1').style.backgroundColor='lightgreen';
+        document.getElementById('w1').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('w1').style.backgroundColor='#808000';
+        document.getElementById('w1').style.color='white';
     }
+    else if (input==""){
+        document.getElementById('w1').style.backgroundColor='white';
+
+    }
+}
+function wiskunde1 () {
+    input = document.getElementById('w2').value;
+    if (input>0 && input < 5.5){
+        document.getElementById('w2').style.backgroundColor='red';
+        document.getElementById('w2').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('w2').style.backgroundColor='lightgreen';
+        document.getElementById('w2').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('w2').style.backgroundColor='#808000';
+        document.getElementById('w2').style.color='white';
+    }
+    else if (input==""){
+        document.getElementById('w2').style.backgroundColor='white';
+
+    }
+}
+function wiskunde2 () {
+    input = document.getElementById('w3').value;
+    if (input>0 && input < 5.5){
+        document.getElementById('w3').style.backgroundColor='red';
+        document.getElementById('w3').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('w3').style.backgroundColor='lightgreen';
+        document.getElementById('w3').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('w3').style.backgroundColor='#808000';
+        document.getElementById('w3').style.color='white';
+    }
+    else if (input==""){
+        document.getElementById('w3').style.backgroundColor='white';
+
+    }
+}
+function wiskunde3 () {
+    input = document.getElementById('w4').value;
+    if (input>0 && input < 5.5){
+        document.getElementById('w4').style.backgroundColor='red';
+        document.getElementById('w4').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('w4').style.backgroundColor='lightgreen';
+        document.getElementById('w4').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('w4').style.backgroundColor='#808000';
+        document.getElementById('w4').style.color='white';
+    }
+    else if (input==""){
+        document.getElementById('w4').style.backgroundColor='white';
+
+    }
+}
+function wiskunde4 () {
+    input = document.getElementById('w5').value;
+    if (input>0 && input < 5.5){
+        document.getElementById('w5').style.backgroundColor='red';
+        document.getElementById('w5').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('w5').style.backgroundColor='lightgreen';
+        document.getElementById('w5').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('w5').style.backgroundColor='#808000';
+        document.getElementById('w5').style.color='white';
+    }
+    else if (input==""){
+        document.getElementById('w5').style.backgroundColor='white';
+
+    }
+}
+
+//Natuurkunde
+function natuur () {
+    input = document.getElementById('n1').value;
+    if (input>0 && input < 5.5){
+        document.getElementById('n1').style.backgroundColor='red';
+        document.getElementById('n1').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('n1').style.backgroundColor='lightgreen';
+        document.getElementById('n1').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('n1').style.backgroundColor='#808000';
+        document.getElementById('n1').style.color='white';
+    }
+    else if (input==""){
+        document.getElementById('n1').style.backgroundColor='white';
+
+    }
+}
+function natuur1 () {
+    input = document.getElementById('n2').value;
+    if (input>0 && input < 5.5){
+        document.getElementById('n2').style.backgroundColor='red';
+        document.getElementById('n2').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('n2').style.backgroundColor='lightgreen';
+        document.getElementById('n2').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('n2').style.backgroundColor='#808000';
+        document.getElementById('n2').style.color='white';
+    }
+    else if (input==""){
+        document.getElementById('n2').style.backgroundColor='white';
+
+    }
+}
+function natuur2 () {
+    input = document.getElementById('n3').value;
+    if (input>0 && input < 5.5){
+        document.getElementById('n3').style.backgroundColor='red';
+        document.getElementById('n3').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('n3').style.backgroundColor='lightgreen';
+        document.getElementById('n3').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('n3').style.backgroundColor='#808000';
+        document.getElementById('n3').style.color='white';
+    }
+    else if (input==""){
+        document.getElementById('n3').style.backgroundColor='white';
+
+    }
+}
+function natuur3 () {
+    input = document.getElementById('n4').value;
+    if (input>0 && input < 5.5){
+        document.getElementById('n4').style.backgroundColor='red';
+        document.getElementById('n4').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('n4').style.backgroundColor='lightgreen';
+        document.getElementById('n4').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('n4').style.backgroundColor='#808000';
+        document.getElementById('n4').style.color='white';
+    }
+    else if (input==""){
+        document.getElementById('n4').style.backgroundColor='white';
+
+    }
+}
+function natuur4 () {
+    input = document.getElementById('n5').value;
+    if (input>0 && input < 5.5){
+        document.getElementById('n5').style.backgroundColor='red';
+        document.getElementById('n5').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('n5').style.backgroundColor='lightgreen';
+        document.getElementById('n5').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('n5').style.backgroundColor='#808000';
+        document.getElementById('n5').style.color='white';
+    }
+    else if (input==""){
+        document.getElementById('n5').style.backgroundColor='white';
+
+    }
+}
+//Nederlands
+function nl1 () {
+    input = document.getElementById('nl1').value;
+    if (input>0 && input < 5.5){
+        document.getElementById('nl1').style.backgroundColor='red';
+        document.getElementById('nl1').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('nl1').style.backgroundColor='lightgreen';
+        document.getElementById('nl1').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('nl1').style.backgroundColor='#808000';
+        document.getElementById('nl1').style.color='white';
+    }
+    else if (input==""){
+        document.getElementById('nl1').style.backgroundColor='white';
+
+    }
+}
+function nl2 () {
+    input = document.getElementById('nl2').value;
+    if (input>0 && input < 5.5){
+        document.getElementById('nl2').style.backgroundColor='red';
+        document.getElementById('nl2').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('nl2').style.backgroundColor='lightgreen';
+        document.getElementById('nl2').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('nl2').style.backgroundColor='#808000';
+        document.getElementById('nl2').style.color='white';
+    }
+    else if (input==""){
+        document.getElementById('nl2').style.backgroundColor='white';
+
+    }
+}
+function nl3 () {
+    input = document.getElementById('nl3').value;
+    if (input>0 && input < 5.5){
+        document.getElementById('nl3').style.backgroundColor='red';
+        document.getElementById('nl3').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('nl3').style.backgroundColor='lightgreen';
+        document.getElementById('nl3').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('nl3').style.backgroundColor='#808000';
+        document.getElementById('nl3').style.color='white';
+    }
+    else if (input==""){
+        document.getElementById('nl3').style.backgroundColor='white';
+
+    }
+}
+function nl4 () {
+    input = document.getElementById('nl4').value;
+    if (input>0 && input < 5.5){
+        document.getElementById('nl4').style.backgroundColor='red';
+        document.getElementById('nl4').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('nl4').style.backgroundColor='lightgreen';
+        document.getElementById('nl4').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('nl4').style.backgroundColor='#808000';
+        document.getElementById('nl4').style.color='white';
+    }
+    else if (input==""){
+        document.getElementById('nl4').style.backgroundColor='white';
+
+    }
+}
+function nl5 () {
+    input = document.getElementById('nl5').value;
+    if (input>0 && input < 5.5){
+        document.getElementById('nl5').style.backgroundColor='red';
+        document.getElementById('nl5').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('nl5').style.backgroundColor='lightgreen';
+        document.getElementById('nl5').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('nl5').style.backgroundColor='#808000';
+        document.getElementById('nl5').style.color='white';
+    }
+    else if (input==""){
+        document.getElementById('nl5').style.backgroundColor='white';
+
+    }
+}
+
+//Engels
+function en1 () {
+    input = document.getElementById('en1').value;
+    if (input>0 && input < 5.5){
+        document.getElementById('en1').style.backgroundColor='red';
+        document.getElementById('en1').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('en1').style.backgroundColor='lightgreen';
+        document.getElementById('en1').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('en1').style.backgroundColor='#808000';
+        document.getElementById('en1').style.color='white';
+    }
+    else if (input==""){
+        document.getElementById('en1').style.backgroundColor='white';
+
+    }
+}
+function en2 () {
+    input = document.getElementById('en2').value;
+    if (input>0 && input < 5.5){
+        document.getElementById('en2').style.backgroundColor='red';
+        document.getElementById('en2').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('en2').style.backgroundColor='lightgreen';
+        document.getElementById('en2').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('en2').style.backgroundColor='#808000';
+        document.getElementById('en2').style.color='white';
+    }
+    else if (input==""){
+        document.getElementById('en2').style.backgroundColor='white';
+    }
+}
+function en3 () {
+    input = document.getElementById('en3').value;
+    if (input>0 && input < 5.5){
+        document.getElementById('en3').style.backgroundColor='red';
+        document.getElementById('en3').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('en3').style.backgroundColor='lightgreen';
+        document.getElementById('en3').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('en3').style.backgroundColor='#808000';
+        document.getElementById('en3').style.color='white';
+    }
+    else if (input==""){
+        document.getElementById('en3').style.backgroundColor='white';
+    }
+}
+function en4 () {
+    input = document.getElementById('en4').value;
+    if (input>0 && input < 5.5){
+        document.getElementById('en4').style.backgroundColor='red';
+        document.getElementById('en4').style.color='white';
+    }else if (input >= 5.5 && input <7.5){
+        document.getElementById('en4').style.backgroundColor='lightgreen';
+        document.getElementById('en4').style.color='white';
+    }else if (input > 7.5 && input<=10){
+        document.getElementById('en4').style.backgroundColor='#808000';
+        document.getElementById('en4').style.color='white';
+    }
+    else if (input==""){
+        document.getElementById('en4').style.backgroundColor='white';
+    }
+}
+function en5 () {
+    inputen5 = document.getElementById('en5').value;
+    if (inputen5>0 && inputen5 < 5.5){
+        document.getElementById('en5').style.backgroundColor='red';
+        document.getElementById('en5').style.color='white';
+    }else if (inputen5 >= 5.5 && inputen5 <7.5){
+        document.getElementById('en5').style.backgroundColor='lightgreen';
+        document.getElementById('en5').style.color='white';
+    }else if (inputen5 > 7.5 && inputen5<=10){
+        document.getElementById('en5').style.backgroundColor='#808000';
+        document.getElementById('en5').style.color='white';
+    }
+    else if (inputen5==""){
+        document.getElementById('en5').style.backgroundColor='white';
+    }
+}
+//Gemiddelde talen cijfer
+
+function gemiddelde( ){
+    nl5();
+    en5 ();
+    let sum =(parseInt(document.getElementById('en5').value) + parseInt(document.getElementById('nl5').value))/2;
+
+    
+    
+    
+    let proef = document.getElementById('garethTaal').innerContent='hola';
+    
+    return sum;
+}
+
