b0eedc51228105913e5b96d537a4efa829043fb5
Borja <borjaneumann@hotmail.com>
2020-10-23 10:31:46 +0200

    Javascript rekenmachine

--- 05_JAVASCRIPT/11_Eindopdracht/rekenmachine.js
+++ 05_JAVASCRIPT/11_Eindopdracht/rekenmachine.js
@@ -1,15 +1,4 @@
-//when I click on a numerical button, the value has to appear on the screen.
-//values should be added as strings 2, 26, 26 when the numerical buttons are clicked
-//collect the values from the buttons and store it in a const
-//Loop the values to simplified previous step.
-//add a behaviour to the buttons
-//make a function that evaluates (probably eval()) the operation that is in the string of the eval function.
-//make a function so = shows eval()
-//make a function to change the sign of the value on the display
-
-//make a function to round the result to a maximum of 2 decimals
-//make a function so 5+ is equal to 5+5 and the same for * and /
-let input= '';
+//Variables declaration 
 let display=document.getElementById('result')
 let result=document.getElementById('equal')
 let allClear = document.getElementById('ac')
@@ -19,30 +8,66 @@ function enterInput(e){
   display.innerHTML=input;
   allClear.innerHTML='C';
 }
-
-
-
-
-
-
-let btnArray= document.querySelectorAll('button')
-
+//collect the values from the buttons and store it in a const
+let btnArray= document.querySelectorAll('button');
+//when I click on a numerical button, the value has to appear on the screen.
 for (i=0;i<btnArray.length;i++){
   if (btnArray[i].innerHTML!='='&& btnArray[i].innerHTML!='AC' && btnArray[i].innerHTML!='+/-'){
     btnArray[i].addEventListener('click',enterInput);
   }
 }
+//Change color operator buttons on click
+let btnOperatorArray=document.getElementsByClassName('white-change');
 
+for (let i=0;i<btnOperatorArray.length;i++){
+  if (btnOperatorArray[i].innerHTML=='/'|| btnOperatorArray[i].innerHTML=='*'||btnOperatorArray[i].innerHTML==='+'||btnOperatorArray[i].innerHTML==='-'){
+  btnOperatorArray[i].addEventListener('click',changeColor)
+  console.log(btnOperatorArray[i].innerHTML)
+  }
+}
+//change operator's color on and off(it works though it is not the goal.)
+// let state = false;
+// function changeColor(element){
+//   state=!state;
+//   let a =document.getElementById(element)
+//   if(state){
+//     a.style.backgroundColor ='white'
+//     a.style.color ='red'}
+//   else {
+//     console.log('hola')
+//     console.log(a.innerHTML == element)
+//     a.style.backgroundColor ='orange';
+//     a.style.color ='black';
+//   } 
+// }
 
-
-
-
+//change operator's color to white
+function changeColor(element){
+  let a =document.getElementById(element);
+  if (a.innerHTML=='/'||a.innerHTML=='*'||a.innerHTML=='-'||a.innerHTML=='+'){
+    a.style.backgroundColor ='white'
+    a.style.color ='red'}
+}
+//change operator's color back to orange
+function backToColor(element){
+  let a =document.getElementById(element);
+  if (a.innerHTML!=='/'||a.innerHTML!=='*'||a.innerHTML!=='-'||a.innerHTML1=='+'){
+    document.getElementById('minus').style.backgroundColor ='orange';
+    document.getElementById('minus').style.color ='black';
+    document.getElementById('many').style.backgroundColor ='orange';
+    document.getElementById('many').style.color ='black';
+    document.getElementById('plus').style.backgroundColor ='orange';
+    document.getElementById('plus').style.color ='black';
+    document.getElementById('division').style.backgroundColor ='orange';
+    document.getElementById('division').style.color ='black';
+  }
+}
+//Appplying addEventListeners to single buttons
 result.addEventListener('click', showResult)
 allClear.addEventListener('click',reset)
-allClear.addEventListener('click',reset2)
 sign.addEventListener('click', signHandler)
 
-
+//add and remove minus symbol
 function signHandler(){
   let negative='-'
   display.innerHTML='';
@@ -56,41 +81,32 @@ function signHandler(){
       input=display.innerHTML; 
     }
 }
-
-//make a function so AC goes back to 0 and turns into C when a value is entered.
-
+//show result on the display, calcultes 5+/* plus = and rounds up to 2 decimals when applicable
 function showResult(){
   display.innerHTML='';
-  if (input!==''){
-    display.innerHTML=eval(input);
+  if(input.charAt(input.length -1)==='+'||input.charAt(input.length -1)==='*'||input.charAt(input.length -1)==='/'){
+    let inputSliced= input.slice(0,input.length -1);
+    console.log(inputSliced)
+    input= input + inputSliced;
+    display.innerHTML= eval(input);
   }
-  else if(input.charAt(input.length -1)='+'){
-    console.log(input.charAt(input.length -1));
-    display.innerHTML= eval((input.slice(0,input.length -1))+(input.slice(0,input.length -1)));
-  }
+  if(input !==''){
+      console.log(display.innerHTML=eval(input))
+      if (eval(input)%2!==0){
+      display.innerHTML=eval(input).toFixed(2);}
+      else{
+        display.innerHTML=eval(input);
+      }
+  } 
 }
-
-
-// function sameNumber(){
-//   display.innerHTML=input;
-//   if(display.slice(-1)='+'){
-//     eval(display-display.length(-1));
-//   }
-
-// }
-
-
+//Makes AC to C when button any button is pushed and when C is clicked resets the display.
 function reset(){
   input=='';
   display.innerHTML='0';
-}
-function reset2(){
   if (allClear.innerHTML== 'C'){
     allClear.innerHTML='AC';
     display.innerHTML='0'
     input='';
   }
-} 
+}
 
-
-  // (Math.round(num + "e+2")  + "e-2")
